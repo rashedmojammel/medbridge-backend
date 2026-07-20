b59c3d0353390a97aabec77bd6e5459f5a725036
Tanvir Ahmed <tanvir.cse2004@gmail.com>
2026-07-21 03:27:54 +0600

    Feat : Added All File

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true, 
+        synchronize: true, 
+      }),
+    }),
+
+    ScheduleModule.forRoot(), 
+    AuthModule,
+    UsersModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
